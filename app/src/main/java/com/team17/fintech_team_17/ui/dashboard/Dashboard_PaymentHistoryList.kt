4e36af5e44db7a08767c839e93d59c6d6179757e
Sel3bRr-T <slebona404@gmail.com>
2022-11-29 15:54:26 +0200

    Should login and go to Dashboard now

---
+++ app/src/main/java/com/team17/fintech_team_17/ui/dashboard/Dashboard_PaymentHistoryList.kt
@@ -0,0 +1,12 @@
+package com.team17.fintech_team_17.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.team17.fintech_team_17.R
+
+class Dashboard_PaymentHistoryList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_payment_history_list)
+    }
+}
--- app/src/main/java/com/team17/fintech_team_17/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/team17/fintech_team_17/ui/register/RegisterActivity.kt
@@ -7,10 +7,15 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.team17.fintech_team_17.R
-import com.team17.fintech_team_17.ui.login.LoginActivity
+import com.team17.fintech_team_17.ui.dashboard.ActivityDashboard
+import com.team17.fintech_team_17.ui.loginTwo.LoginTwoActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var rAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -23,8 +28,12 @@ class RegisterActivity : AppCompatActivity() {
         val btnSignup = findViewById<Button>(R.id.btnsignup) as Button;
         val btnSignin = findViewById<Button>(R.id.btnsignin) as Button;
 
+        rAuth = FirebaseAuth.getInstance()
+
 
         btnSignup.setOnClickListener{
+
+            //Add user to user dataclass
             val name = edtUsername.text.toString()
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
@@ -39,24 +48,63 @@ class RegisterActivity : AppCompatActivity() {
             }
             else{
                 val usr = UserModel(name = name, email = email)
-                Log.i("Register","usr made: " + usr.id)
+                Log.i("Register","usr made: " + usr.id + " : " + usr.name)
+
+                //Add user to firebase database
+                /*
+                FirebaseConnect.putUser(usr.id.toString(), usr.name, usr.email, password)
+                FirebaseConnect.addUser()
+
+                 */
+
+                //Add user to Firebase Authentication
+                val dashboardIntent = Intent(this, ActivityDashboard::class.java)
+                val success = createUser(usr.email, edtPassword.text.toString(), dashboardIntent)
+                if (success == true) {
+                    startActivity(dashboardIntent)
+                }
 
 
                 //Show message for successful registration
-                Toast.makeText(this, "User Registered!", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "User Registered!", Toast.LENGTH_SHORT).show()
 
-                //Return to Login screen
-                val loginIntent = Intent(this, LoginActivity::class.java)
-                startActivity(loginIntent)
-                finish()
+
             }
         }
 
         btnSignin.setOnClickListener{
-            val loginIntent = Intent(this, LoginActivity::class.java)
+            val loginIntent = Intent(this, LoginTwoActivity::class.java)
             startActivity(loginIntent)
             finish()
         }
+    }
+
+    fun createUser(email: String, password: String, nextActivity: Intent): Boolean {
+        var created = false
+        rAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT)
+                        .show()
+                    val user = rAuth.currentUser
+                    created = true
+
+                    startActivity(nextActivity)
+                    finish()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
+                }
+
+            }
+        return created
     }
 
 
